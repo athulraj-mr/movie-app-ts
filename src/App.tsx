5b8b099bd970435746da283fa6e602f0a179b5f0
Cloaked-Walker <gouthama921@gmail.com>
2025-11-20 11:31:38 +0530

    perf: add debounced search using react-use
    - Add useDebounce from react-use package
    - Optimize search API call frequency
    - Enhance typing performance with debouncing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useDebounce } from 'react-use';
 import Search from './components/Search'
 import Spinner from './components/Spinner';
 import type { Movie, MovieApiResponse } from './types/movie';
@@ -21,6 +22,9 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [movieList, setMovieList] = useState<Movie[]>([]);
   const [isloading, setIsLoading] = useState<boolean>(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce(()=> setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query= ''): Promise<void> => {
     setIsLoading(true);
@@ -50,7 +54,7 @@ const App = () => {
 
   useEffect(() => {
     fetchMovies(searchTerm);
-  },[searchTerm]);
+  },[debouncedSearchTerm]);
 
   return (
     <main>
