5326da74412ce12c77982816d5ab7910db8ec37d
Cloaked-Walker <gouthama921@gmail.com>
2025-11-20 10:46:59 +0530

    feat: add search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,14 @@ const App = () => {
   const [movieList, setMovieList] = useState<Movie[]>([]);
   const [isloading, setIsLoading] = useState<boolean>(false);
 
-  const fetchMovies = async (): Promise<void> => {
+  const fetchMovies = async (query= ''): Promise<void> => {
     setIsLoading(true);
     setErrorMessage('');
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -47,8 +49,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  },[]);
+    fetchMovies(searchTerm);
+  },[searchTerm]);
 
   return (
     <main>
