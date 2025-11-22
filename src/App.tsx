28eeb59e82bf816dcd03e90448789741dcc45ea6
Cloaked-Walker <gouthama921@gmail.com>
2025-11-22 11:24:09 +0530

    style(movie-detail): final UI updates to movie detail page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import MovieDetails from './components/MovieDetails';
 import { useMovies } from './hooks/useMovies';
+import hero from '../public/hero.png'
 
 
 const App = () => {
@@ -19,7 +20,7 @@ const App = () => {
 
             <div className='wrapper'>
               <header>
-                <img src="/hero.png" alt="Hero Banner" />
+                <img src={hero} alt="Hero Banner" />
                 <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
 
                 <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -5,12 +5,20 @@ export interface Movie {
   poster_path: string | null;
   backdrop_path: string | null;
   vote_average: number;
+  vote_count: number;
   release_date: string;
   original_language: string;
+  original_title: string;
   genre_ids?: number[];
+  genres?: Genre[];
   popularity: number;
   adult: boolean;
   video: boolean;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
 }
 
 export interface MovieApiResponse {
